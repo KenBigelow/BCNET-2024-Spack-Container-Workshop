30fafca55437520d70057bd825109ad5d2bb2c03
KenBigelow <44417562+KenBigelow@users.noreply.github.com>
2024-03-11 21:02:18 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,11 +1,5 @@
 # Configuration file for the Sphinx documentation builder.
 
-# -- Project information
-
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
-
 release = '0.1'
 version = '0.1.0'
 
@@ -30,6 +24,3 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
-
-# -- Options for EPUB output
-epub_show_urls = 'footnote'

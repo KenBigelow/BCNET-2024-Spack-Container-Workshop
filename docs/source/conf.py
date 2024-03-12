8701f40dadbae731ecd7ad525441405cc0aaaa67
KenBigelow <44417562+KenBigelow@users.noreply.github.com>
2024-03-12 09:40:53 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,13 +27,13 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_logo = "bcnet-logo.png"
 html_theme_options = {
-    "logo_only": True,
-    "display_version": False,     
-    "collapse_navigation": False,
-    "sticky_navigation": True,
-    "navigation_depth": 4,
-    "includehidden": True,
-    "titles_only": False,
-    "globaltoc_collapse": True,
-    "globaltoc_maxdepth": None,
+    'logo_only': True,
+    'display_version': False,     
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False,
+    'globaltoc_collapse': True,
+    'globaltoc_maxdepth': None,
 }
